40d7e821417b26b1d36f3aa33bf04a2b321b3f04
FrancescoCialfiDev <cialfifrancesco02@gmail.com>
2025-03-19 18:06:36 +0100

    finished exercices

--- script.js
+++ script.js
@@ -31,4 +31,58 @@ function getPost(id) {
         })
         .catch((error) => console.error(error))
 }
-getPost(1) 
+getPost(1)
+
+// SECONDO ESERCIZIO
+
+/*Crea la funzione lanciaDado() che restituisce una Promise che, dopo 3 secondi, genera un numero casuale tra 1 e 6. Tuttavia, nel 20% dei casi, il dado si "incastra" e la Promise va in reject.
+🎯 Bonus: HOF con closure per memorizzare l'ultimo lancio
+Modifica la funzione in creaLanciaDado(), che restituisce una closure che memorizza l'ultimo risultato. Se il numero esce due volte di fila, stampa "Incredibile!".*/
+
+const numeroGenerato = document.getElementById("numeroGenerato")
+const bottone = document.getElementById("bottone")
+bottone.addEventListener("click", () => {
+    news()
+})
+
+function lanciaDado() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const randomCrash = Math.floor(Math.random() * 5) + 1
+            if (randomCrash != 1) {
+                const randomNumber = Math.floor(Math.random() * 6) + 1
+                resolve(randomNumber)
+            } else {
+                reject("Il dado si è incastrato")
+            }
+        }, 500)
+    })
+}
+
+// SECONDO ESERCIZIO BONUS
+
+function creaLanciaDado() {
+    let ultimoLancio = null
+    return function () {
+        lanciaDado()
+            .then((numero) => {
+                console.log(`Il numero generato è ${numero}`)
+                numeroGenerato.innerHTML = `Il numero generato è ${numero}`
+                if (ultimoLancio === numero) {
+                    numeroGenerato.innerHTML = `Incredibile`
+                    console.log("Incredibile!");
+                }
+                ultimoLancio = numero;
+            })
+            .catch(err => { console.error(err); numeroGenerato.innerHTML = "Il dado si è incastrato"; ultimoLancio = null })
+    }
+}
+
+const news = creaLanciaDado()
+
+
+
+
+
+
+
